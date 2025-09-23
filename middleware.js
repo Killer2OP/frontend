be2ab33dec6f34c17dc070532bac0ee135bd20cd
Pratham Mahajan <111378171+Killer2OP@users.noreply.github.com>
2025-09-23 21:18:46 +0530

    h

--- middleware.js
+++ middleware.js
@@ -41,11 +41,10 @@ export function middleware(req) {
       allCookies: Object.keys(req.cookies.getAll())
     });
 
-    // For now, allow access to /admin even without token (temporary fix)
-    // TODO: Remove this after fixing cookie issues
     if (!adminToken) {
-      console.log('WARNING: Allowing admin access without token (temporary bypass)');
-      return NextResponse.next();
+      const url = req.nextUrl.clone();
+      url.pathname = '/admin/login';
+      return NextResponse.redirect(url);
     }
 
     return NextResponse.next();
